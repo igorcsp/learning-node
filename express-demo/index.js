393eabe8251f41e40007facc3c594afced58844a
igorcsp <icordeiro787@gmail.com>
2024-08-18 15:01:18 -0300

    .

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,122 +1,35 @@
 const debug = require('debug')('app:startup')
-
-// const startupDebugger = require('debug')('app:startup')
-// const dbDebugger = require('debug')('app:db')
-// to enable use `export DEBUG=app:startup,app:db` ou `export DEBUG=app:*`, to disable `export DEBUG=`
-// nice shortcut example > `export DEBUG=app:db nodemon index.js`
-
-// const config = require('config')
+const config = require('config')
+const helmet = require('express')
+const logger = require('./middleware/logger')
+const morgan = require('morgan')
 const express = require('express')
 const app = express()
-const Joi = require('joi')
-// const logger = require('./logger')
-const helmet = require('express')
-const morgan = require('morgan')
+const courses = require('./routes/courses')
+const home = require('./routes/home')
 
-// Configuration
-// console.log('Aplication Name: ' + config.get('name'))
-// console.log('Mail Server: ' + config.get('mail.host'))
-// console.log('Mail Password: ' + config.get('mail.password'))
+app.set('view engine', 'pug')
+app.set('views', './views') // default
 
 app.use(express.json())
-// app.use(express.urlencoded({ extended: true }))
-// app.use(express.static('public'))
-app.use(helmet()); // Helps secure your apps by setting various HTTP headers.
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'))
+app.use(helmet());
+
+app.use('/', home)
+app.use('/api/courses', courses)
+
+// Configuration
+console.log('Aplication Name: ' + config.get('name'))
+console.log('Mail Server: ' + config.get('mail.host'))
+console.log('Mail Password: ' + config.get('mail.password'))
+
+console.log(logger)
 
 if (app.get('env') === 'development') {
-    app.use(morgan('tiny')); // HTTP request logger.
-    // startupDebugger('Morgan enabled...')
+    app.use(morgan('tiny'))
     debug('Morgan enabled...')
 }
 
-//DB work...
-// dbDebugger('Connectted to the database...')
-
-// app.use(logger.log)
-
-function validateCourse(course) {
-    const schema = Joi.object({
-        name: Joi.string()
-            .min(3)
-            .max(45)
-            .required(),
-    })
-
-    return schema.validate({ name: course })
-}
-
-let courses = [
-    { id: 1, name: 'HTML' },
-    { id: 2, name: 'CSS' },
-    { id: 3, name: 'Javascript' },
-]
-
-// GET
-app.get('/', (req, res) => {
-    res.send('Home page')
-})
-
-app.get('/api/courses', (req, res) => {
-    res.send(courses)
-})
-
-app.get('/api/courses/:id', (req, res) => {
-    const course = courses.find((c) => c.id === parseInt(req.params.id))
-    if (!course) return res.status(404).send(`The course of the given id of ${req.params.id} was not found`)
-    res.send(course)
-})
-
-// POST
-app.post('/api/courses', (req, res) => {
-    const { error } = validateCourse(req.body.name)
-
-    if (error) return res.status(400).send(error.details[0].message)
-
-    const course = {
-        id: courses.length + 1,
-        name: req.body.name
-    }
-    courses.push(course)
-    res.send(course)
-})
-
-// PUT
-app.put('/api/courses/:id', (req, res) => {
-    const course = courses.find(c => c.id === parseInt(req.params.id))
-    if (!course) return res.status(404).send(`Course of id ${req.params.id} not found`)
-
-    const { error } = validateCourse(req.body.name)
-
-    if (error) return res.status(400).send(error.details[0].message)
-
-
-    course.name = req.body.name
-    res.send(course)
-})
-
-// DELETE
-// my first solution
-// app.delete('/api/courses/:id', (req, res) => {
-//     const newArrCourses = courses.filter((c) => c.id !== parseInt(req.params.id))
-
-//     if (parseInt(req.params.id) !== courses.length) return res.status(404).send("ID not found")
-
-//     courses = [...newArrCourses]
-//     res.send(newArrCourses)
-// })
-
-// course solution
-app.delete('/api/courses/:id', (req, res) => {
-    const course = courses.find(c => c.id === parseInt(req.params.id))
-    if (!course) return res.status(404).send(`Course of id ${req.params.id} not found`)
-
-    const index = courses.indexOf(course)
-    courses.splice(index, 1)
-
-    res.send(course)
-})
-
-
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`Listening on port ${port}...`))
-// para configurar a variável de ambiente no CLI > `export PORT=5000` no BASH e `set PORT=5000` no windowns
--- vidly/index.js
+++ vidly/index.js
@@ -1,71 +1,11 @@
+const router = require('./routes/genres')
 const express = require('express')
-const Joi = require('joi')
 const app = express()
 
 app.use(express.json())
+app.use(express.json())
+app.use('/api/genres', router)
 
 const port = process.env.PORT || 3000
-
-function validateInput(genre) {
-    const schema = Joi.object({
-        genre: Joi.string().min(3).max(45).required()
-    })
-    return schema.validate({ genre: genre })
-}
-
-const genres = [
-    { id: 1, genre: "horror" },
-    { id: 2, genre: "comedy" },
-    { id: 3, genre: "action" },
-]
-
-//GET
-app.get('/api/genres', (req, res) => {
-    res.send(genres)
-})
-
-
-app.get('/api/genres/:id', (req, res) => {
-    const genre = genres.find(c => c.id === parseInt(req.params.id))
-    if (!genre) return res.status(404).send(`Genre id of ${parseInt(req.params.id)} not found`)
-
-    res.send(genre)
-})
-
-//POST
-app.post('/api/genres', (req, res) => {
-    const { error } = validateInput(req.body.genre)
-    if (error) return res.status(400).send(error.details[0].message)
-
-    const genre = {
-        id: genres.length + 1,
-        genre: req.body.genre
-    }
-
-    genres.push(genre)
-    res.send(genre)
-})
-
-//PUT
-app.put('/api/genres/:id', (req, res) => {
-    const genre = genres.find(c => c.id === parseInt(req.params.id))
-    if (!genre) return res.status(404).send(`Genre id of ${parseInt(req.params.id)} not found`)
-
-    const { error } = validateInput(req.body.genre)
-    if (error) return res.status(400).send(error.details[0].message)
-
-    genre.name = req.body.name;
-    res.send(genre);
-})
-
-app.delete('/api/genres/:id', (req, res) => {
-    const genre = genres.find(c => c.id === parseInt(req.params.id))
-    if (!genre) return res.status(404).send(`Genre id of ${parseInt(req.params.id)} not found`)
-
-    const index = genres.indexOf(genre)
-    genres.splice(index, 1)
-
-    res.send(genre)
-})
 
 app.listen(port, () => console.log(`Listening on port ${port}...`))
